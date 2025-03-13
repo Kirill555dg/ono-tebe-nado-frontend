84666cfcf735caf362ce6a57a5f7fb8e464972e7
Kerrodar <Kirill555dg@gmail.com>
2025-03-13 11:08:20 +0300

    style: fix indentations

--- src/components/view/Order.ts
+++ src/components/view/Order.ts
@@ -3,15 +3,15 @@ import { IEvents } from "../base/events";
 import { Form } from "../common/Form";
 
 export default class Order extends Form<IOrderForm> {
-    constructor(container: HTMLFormElement, events: IEvents) {
-        super(container, events);
-    }
+  constructor(container: HTMLFormElement, events: IEvents) {
+    super(container, events);
+  }
 
-    set phone(value: string) {
-        (this.container.elements.namedItem('phone') as HTMLInputElement).value = value;
-    }
+  set phone(value: string) {
+    (this.container.elements.namedItem('phone') as HTMLInputElement).value = value;
+  }
 
-    set email(value: string) {
-        (this.container.elements.namedItem('email') as HTMLInputElement).value = value;
-    }
+  set email(value: string) {
+    (this.container.elements.namedItem('email') as HTMLInputElement).value = value;
+  }
 }
